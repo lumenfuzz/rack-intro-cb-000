54542dcc6654693730ced486ab9ea0644f5a8aa1
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-20 16:18:47 +0000

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,10 @@
 class Application
 
   def call(env)
+    name = "computer"
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is " + name
     resp.finish
   end
 
 end
-
